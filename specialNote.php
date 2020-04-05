a797bc9db4d751baffee4260aedf52beeb5c8ed3
ngels <pamyngels@outlook.fr>
2020-04-05 14:29:55 +0800

    add some simple ressources

---
+++ specialNote.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="resources/style/beautifull.css">
+</head>
+<body>
+<!-- Note -->
+<div style="background:yellow;padding:2px">
+  <h4 style="text-align:center">Welcome to the best event planner in the country</h4>
+</div>
+</body>
